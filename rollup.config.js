e26f0aa7b627d70635ccdbf9301d12483de4b6ce
yumingt <33796267+yumingt@users.noreply.github.com>
2023-02-21 09:46:43 -0800

    added all files to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ module.exports = {
   treeshake: false,
   plugins: [
     copy({
-      targets: [{ src: "index.html", dest: "dist" }],
+      targets: [{ src: ["index.html", "p5.play.js", "player.js", "index.js", "blob.js", "style.css", "1pot.PNG", "background.PNG", "box cat.PNG", "game over black.png", "ground.PNG"], dest: "dist" }],
     }),
   ],
 };
